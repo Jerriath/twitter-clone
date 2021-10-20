9ff71da7273d868a135d32f5a8259b7a6af419d7
Jerry Zhang <jerryzhang101@yahoo.com>
2021-10-20 18:04:30 -0400

    Added right panel and signup slide

--- src/page-components/home-page/home-subcomponents/RightPanel.js
+++ src/page-components/home-page/home-subcomponents/RightPanel.js
@@ -10,7 +10,15 @@ const RightPanel = () => {
 
     return (
         <div className="rightPanel" >
-
+            <div className="rightSpacing">
+                <div className="rightSignup">
+                    <h2 className="titleFont">New to Tweeter?</h2>
+                    <p className="defaultFont">Sign up now to get the same timeline as everyone else.</p>
+                    <div className="redButton">
+                        <h2 className="buttonFont">Sign Up</h2>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
