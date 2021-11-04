445f5420ff00412598308c00d01be84cef45a1fe
Jerry Zhang <jerryzhang101@yahoo.com>
2021-11-04 14:37:04 -0400

    Added tweet input component and fiddled with styling

--- src/page-components/home-page/home-subcomponents/TweetInput.js
+++ src/page-components/home-page/home-subcomponents/TweetInput.js
@@ -13,7 +13,15 @@ const TweetInput = (props) => {
     return (
         <div className="greyScreen">
             <div className="tweetInput" >
-
+                <div className="tweetHeader" >
+                    <button className="closeForm" >x</button>
+                </div>
+                <hr className="line" />
+                <form className="tweetForm" >
+                    <label className="tweetLabel" >
+                        <input className="tweetText" type="text" placeholder="What's Happening?" />
+                    </label>
+                </form>
             </div>
         </div>
     )
